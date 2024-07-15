1f18f4b4fc8418a1952dbf9658d65cff9d2ca82b
José Beltrán <jose.beltran@nateko.lu.se>
2024-07-15 15:58:49 +0200

    WIP sites_spectral_core

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,1 +1,5 @@
 ## [v0.1.0]
+- bare bones 
+
+##[v0.1.1]
+- Added `sites_spectral_core` module. The module has `stations`, `ecosystems` dictionaries. 
---
+++ src/sstc_core/sites_spectral_core.py
@@ -0,0 +1,396 @@
+stations = {
+    "Abisko": {
+        "acronym": "ANS",
+        "name": "Abisko Scientific Research Station",
+        "is_active": True,
+        "locations":
+           {               
+               1:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "OPH-1",
+                   "description": "Phenocam Area 1, P01-ROI1-LG",
+                   "ecosystems":[]
+               },
+               2:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "OPH-2",
+                   "description": "Phenocam Area 2, P01-ROI2-R",
+                   "ecosystems":[]
+               },
+               3:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "OPH-3",
+                   "description": "Phenocam Area 3, P01-ROI3-B",
+                   "ecosystems":[]
+               },
+               4:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "OPH-4",
+                   "description": "Phenocam Area 4, P01-ROI4-LB",
+                   "ecosystems":[],
+               5:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "OPH-5",
+                   "description": "Phenocam Area 5, P01-ROI5-P",
+                   "ecosystems":[]
+               },               
+               6:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "M45P01",
+                   "description": "mast 4.5m Phenocam 01, P01 center",
+                   "ecosystems":[]
+               },
+               7:{
+                   "name": "Stordalen Mire Spectral West",
+                   "acronym": "SSW",
+                   "description": "",
+                   "ecosystems":["MIR"]
+               },
+               8:{
+                   "name": "Stordalen Mire Spectral East",
+                   "acronym": "SSE",
+                   "description": "",
+                   "ecosystems":["MIR"]
+               },
+               9:{
+                   "name": "Stordalen Mire Spectral",
+                   "acronym": "SMS",
+                   "description": "",
+                   "ecosystems":["MIR"]
+               },
+               10:{
+                   "name": "Stordalen Mire Spectral West_Sky-01",
+                   "acronym": "SWS01",
+                   "description": "Sky_SWE-ANS-SSW-MIR-F01-01",
+                   "ecosystems":["MIR"]
+               },
+               11:{
+                   "name": "Stordalen Mire Spectral West_Sky-02",
+                   "acronym": "SWS02",
+                   "description": "Sky_SWE-ANS-SSW-MIR-F01-02_2020",
+                   "ecosystems":["MIR"]
+               },
+               12:{
+                   "name": "Stordalen Mire Spectral East_Decagon_01",
+                   "acronym": "SED01",
+                   "description": "Decagon_SWE-ANS-SSE-MIR-F02-01",
+                   "ecosystems":["MIR"]
+               },
+               13:{
+                   "name": "Stordalen Mire Spectral East_Decagon_02",
+                   "acronym": "SED02",
+                   "description": "Decagon_SWE-ANS-SSE-MIR-F02-02_2020",
+                   "ecosystems":["MIR"]
+               },
+              14:{
+                   "name": "Stordalen Mire Spectral East_Sky_01",
+                   "acronym": "SES01",
+                   "description": "Sky_SWE-ANS-SSE-MIR-F02-01",
+                   "ecosystems":["MIR"]
+               },
+              15:{
+                   "name": "Stordalen Mire Spectral East_Sky_02",
+                   "acronym": "SES02",
+                   "description": "Sky_SWE-ANS-SSE-MIR-F02-02_2020",
+                   "ecosystems":["MIR"]
+               },
+              
+              16:{
+                   "name": "Miellejohka",
+                   "acronym": "MKA",
+                   "description": "UAV Miellejohka2020",
+                   "ecosystems":["HEA"]
+               },
+                                
+              17:{
+                   "name": "Stordalen Forest",
+                   "acronym": "SUF",
+                   "description": "UAV Stordalen_Forest2020",
+                   "ecosystems":["FOR"]
+               },
+                                
+              18:{
+                   "name": "Stordalen UAV Mire ",
+                   "acronym": "SUM",
+                   "description": "UAV abiskoMire2020",
+                   "ecosystems":["MIR"]
+               },
+              
+              19:{
+                   "name": "Stordalen Mire North",
+                   "acronym": "STO-MIR-North-UAV",
+                   "description": "UAV 2024",
+                   "ecosystems":["MIR"]
+               },    
+
+              20:{
+                   "name": "Stordalen Mire Center",
+                   "acronym": "STO-MIR-Center-UAV",
+                   "description": "UAV 2024",
+                   "ecosystems":["MIR"]
+               },    
+    
+              21:{
+                   "name": "Stordalen Mire South",
+                   "acronym": "STO-MIR-South-UAV",
+                   "description": "UAV 2024",
+                   "ecosystems":["MIR"]
+               },                   
+               
+               },
+               
+               } 
+        }, 
+    "Asa":{
+        "acronym": "ASA",
+        "name": "Asa Research Station",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Nybygget",
+                   "acronym": "NYB",
+                   "description": "",
+                   "ecosystems":["CON"]
+               },
+                2:{
+                   "name": "Nybygget Fixed Sensor 01",
+                   "acronym": "NYF-01",
+                   "description": "SWE-ASA-NYB-FOR-F01-01",
+                   "ecosystems":["CON"]
+               },                
+                3:{
+                   "name": "Nybygget Fixed Sensor 02",
+                   "acronym": "NYF-02",
+                   "description": "SWE-ASA-NYB-FOR-F01-02",
+                   "ecosystems":["CON"]
+               },
+                4:{
+                   "name": "Nybygget Phenocam 01",
+                   "acronym": "NYP-01",
+                   "description": "SP01",
+                   "ecosystems":["CON"]
+               },                
+                5:{
+                   "name": "ASA UAV area before 2020",
+                   "acronym": "ASU asa2020",
+                   "description": "ASU(past)",
+                   "ecosystems":["CON"]
+               },
+                6:{
+                   "name": "ASA UAV area before 2021",
+                   "acronym": "ASU asa2021",
+                   "description": "ASU(Prsent)",
+                   "ecosystems":["CON"]
+               },
+                7:{
+                   "name": "ASA UAV area 2024",
+                   "acronym": "ASU01",
+                   "description": "",
+                   "ecosystems":["CON"]
+               },                         
+            
+           }
+           },
+    "Bolmen":{
+        "acronym": "BOL",
+        "name": "Bolmen Research Station",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Bolmen Research Station",
+                   "acronym": "BOL",
+                   "description": "",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    "Erken":{
+        "acronym": "ERK",
+        "name": "Erken Laboratory",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Erken",
+                   "acronym": "Erken",
+                   "description": "",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    "Grimsö":{
+        "acronym": "GRI",
+        "name": "Grimsö Wildlife Research Station",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Grimsö Wildlife Research Station",
+                   "acronym": "GRI",
+                   "description": "Spectral Camera",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    "Lönnstorp":{
+        "acronym": "LON",
+        "name": "Lönnstorp Reasearch Station",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Mast 10m",
+                   "acronym": "SFA-M10F1",
+                   #todo: change acronym similar to the Mast
+                   "description": "Lönnstorp Experimental Fields, SWE-LON-SFA-AGR-F01-01, Sensor 01, M10F1",
+                   "ecosystems":["AGR"]
+               },
+                2:{
+                   "name": "Mast 10m",
+                   "acronym": "SFA-M10F2",
+                   "description": "Lönnstorp Experimental Fields, SWE-LON-SFA-AGR-F01-02, Sensor 02, M10F2",
+                   "ecosystems":["AGR"]
+               },
+                3:{
+                   "name": "Mast 10m",
+                   "acronym": "SAFE-M10",
+                   "description": "Lönnstorp Experimental Fields, SAFE M10M",
+                   "ecosystems":["AGR"]
+               },
+                4:{
+                   "name": "Mast 4m",
+                   "acronym": "SFA-M4M",
+                   "description": "Lönnstorp Experimental Fields, SAFE, Mast 4m, Follows Winter weat in the crop rotation",
+                   "ecosystems":["AGR"]
+               },
+                5:{
+                   "name": "Mast 4m fixed sensor",
+                   "acronym": "SFA-M4F",
+                   "description": "Lönnstorp Experimental Fields, SWE-LON-SFA-AGR-F02, Mast 4m fixed sensor",
+                   "ecosystems":["AGR"]
+               },                                
+                6:{
+                   "name": "Mast 10m",
+                   "acronym": "M10P01",
+                   "description": "Lönnstorp Experimental Fields, Mast 10m Penocam 01",
+                   "ecosystems":["AGR"]
+               },
+                7:{
+                   "name": "Mast 10m",
+                   "acronym": "M10P02",
+                   "description": "Lönnstorp Experimental Fields, Mast 10m Penocam 02",
+                   "ecosystems":["AGR"]
+               },
+                8:{
+                   "name": "Mast 10m",
+                   "acronym": "M10P03",
+                   "description": "Lönnstorp Experimental Fields, Mast 10m Penocam 03",
+                   "ecosystems":["AGR"]
+               },
+                9:{
+                   "name": "ROI UAV",
+                   "acronym": "EKO",
+                   "description": "Organic Experimental Field UAV, UAV LonEKO2020",
+                   "ecosystems":["AGR"]
+               },
+                10:{
+                   "name": "ROI UAV",
+                   "acronym": "SFA",
+                   "description": "UAV SAFE A 2020",
+                   "ecosystems":["AGR"]
+               },
+                11:{
+                   "name": "ROI UAV",
+                   "acronym": "SFB",
+                   "description": "UAV SAFE B 2020",
+                   "ecosystems":["AGR"]
+               },
+                12:{
+                   "name": "ROI UAV",
+                   "acronym": "SFAB",
+                   "description": "UAV SAFE AB2021",
+                   "ecosystems":["AGR"]
+               },
+                
+           }
+           },
+    "Röbacksdalen":{
+        "acronym": "RBD",
+        "name": "Röbacksdalen Field Research Station",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "ANS",
+                   "description": "",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    "Skögaryd":{
+        "acronym": "SKC",
+        "name": "Skogaryd Research Catchment",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "ANS",
+                   "description": "",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    "Svartberget":{
+        "acronym": "SVB",
+        "name": "Svartberget Research Station",
+        "is_active": True,
+        "locations":
+           {
+                1:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "ANS",
+                   "description": "",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    "Tarfala":{
+        "acronym": "TRS",
+        "name": "Tarfala Research Station",
+        "is_active": False,
+        "locations":
+           {
+                1:{
+                   "name": "Abisko Scientific Research Station",
+                   "acronym": "ANS",
+                   "description": "",
+                   "ecosystems":[]
+               },
+
+           }
+           },
+    }
+
+ecosystems ={
+    "HEA":{"description": "Heathland"},
+    "AGR":{"description": "Arable land"},
+    "MIR":{"description": "Mires"},
+    "LAK":{"description": "Lake"},
+    "WET":{"description": "Wetland"},
+    "GRA":{"description": "Grassland"},
+    "FOR":{"description": "Forest"},
+    "ALP":{"description": "Alpine forest"},
+    "CON":{"description": "Coniferous forest"},
+    "DEC":{"description": "Deciduous forest"},       
+} 
+
